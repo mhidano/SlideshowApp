f9f39b685c16111f22f293ba3c60044d6e496c6c
mhidano <mhidano0914@gmain.com>
2021-05-11 09:47:46 +0900

    【対応要件】 ・スライドショー画面には、画像と3つのボタン（進む、戻る、再生/停止）を配置してください ・進むボタンで1つ先の画像を表示し、戻るボタンで1つ前の画像を表示します ・最後の画像の表示時に、進むボタンをタップすると、最初の画像が表示されるようにしてください ・最初の画像の表示時に、戻るボタンをタップすると、最後の画像が表示されるようにしてください ・戻るボタンがタップされると、スライドショー画面に戻り、同じ画像を表示してください ・画像は、Xcodeのナビゲーションエリア（Project Navigator）へドラッグアンドドロップし、Copy items if neededにチェックを入れて、Xcodeプロジェクトへコピーしてください

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var onPreview: UIButton!
+    @IBOutlet weak var onNext: UIButton!
+    
+    @IBAction func onPreview(_ sender: Any) {
+        // 表示している画像のインデックスを1減らす
+        dispImageNo -= 1
+        // 画像更新
+        displayImage()
+    }
+    
+    @IBAction func onNext(_ sender: Any) {
+        // 表示している画像のインデックスを1増やす
+        dispImageNo += 1
+        // 画像更新
+        displayImage()
+    }
+    @IBAction func tapImageView(_ sender: Any) {
+        
+    }
+    
+    // 表示している画像のインデックス宣言
+    var dispImageNo = 0
+    
+    // 表示している画像のインデックスを元に画像を更新する
+    func displayImage() {
+        // 画像の名前の配列
+        let imageNameArray = [
+            "pexels-koolshooters-7329628",
+            "pexels-koolshooters-7329629",
+            "pexels-koolshooters-7329631",
+            "pexels-koolshooters-7329634",
+            "pexels-koolshooters-7329635",
+        ]
+        //【配列の範囲チェック】
+        // ・範囲より下を指している場合、最後の画像を表示
+        if dispImageNo < 0 {
+            dispImageNo = 4
+        }
+        // ・範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 4 {
+            dispImageNo = 0
+        }
+        // 表示している画像のインデックスから名前を取り出す
+        let name = imageNameArray[dispImageNo]
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        // Image Viewに読み込んだ画像をセット
+        imageView.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 本プロジェクトにバンドルした画像ファイルを読み込み
+        let image = UIImage(named: "pexels-koolshooters-7329628")
+        // Image Viewに画像を設定
+        imageView.image = image
     }
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
 
 }
 
